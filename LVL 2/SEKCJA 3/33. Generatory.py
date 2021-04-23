0851bdcd2fcc8334d0399b725746e208aaca2fcd
Szymon Hadada <szymon.hadada@gmail.com>
2021-04-23 02:22:03 +0200

    add lesson & LAB "Generatory"

---
+++ LVL 2/SEKCJA 3/33. Generatory.py
@@ -0,0 +1,42 @@
+listA = list(range(6))
+listB = list(range(6))
+
+print(listA,listB)
+
+product = []
+
+for a in listA:
+    for b in listB:
+        product.append((a,b))
+
+print(product)
+
+product = [(a,b) for a in listA for b in listB]
+print(product)
+
+product = [(a,b) for a in listA for b in listB if a % 2 == 1 and b % 2 == 0]
+print(product)
+
+product = {a:b for a in listA for b in listB if a % 2 == 1 and b % 2 == 0}
+print(product)
+
+gen = ((a,b) for a in listA for b in listB if a % 2 == 1 and b % 2 == 0)
+print(gen)
+
+print(next(gen))
+print(next(gen))
+print('-'*30)
+for x in gen:
+    print(x)
+print('*'*30)
+for x in gen:
+    print(x)
+
+gen = ((a,b) for a in listA for b in listB if a % 2 == 1 and b % 2 == 0)
+while True:
+    try:
+
+        print(next(gen))
+    except StopIteration:
+        print("all values have been generated")
+        break
---
+++ LVL 2/SEKCJA 3/34. Generatory - LAB.py
@@ -0,0 +1,34 @@
+ports = ['WAW', 'KRK', 'GDN', 'KTW', 'WMI', 'WRO', 'POZ', 'RZE', 'SZZ',
+         'LUZ', 'BZG', 'LCJ', 'SZY', 'IEG', 'RDO']
+
+lines1 = ( (p,k) for p in ports for k in ports)
+print("lines1")
+counter = 0
+for (p,k) in lines1:
+    print("{} - {}".format(p,k))
+    counter +=1
+
+print(counter)
+
+lines2 = ((p,k) for p in ports for k in ports if p != k)
+print("lines2")
+counter = 0
+for (p,k) in lines2:
+    print("{} - {}".format(p,k))
+    counter +=1
+
+print(counter)
+
+#błąd
+lines3 = {p:k for p in ports for k in ports if p != k}
+print("lines3")
+print(lines3)
+
+lines4 = ((p,k) for p in ports for k in ports if p < k)
+print("lines4")
+counter = 0
+for (p,k) in lines4:
+    print("{} - {}".format(p,k))
+    counter +=1
+
+print(counter)
